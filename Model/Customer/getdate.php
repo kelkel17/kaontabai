5b9b34682684ce78bbd489eb39815297e6f9325c
kelkel17 <saturre.mic2@gmail.com>
2018-02-19 06:06:58 +0800

    new sample

---
+++ Model/Customer/getdate.php
@@ -0,0 +1,12 @@
+<?php
+    include('../Controller/dbconn.php');
+    $con = con();
+    $id = 1;
+    $sql = "SELECT sched_sdate as dat FROM schedules WHERE status = 1 AND restaurant_id = 1";
+    $stmt = $con->prepare($sql);
+    $stmt->execute();
+    $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+   
+    echo json_encode($row);
+
+?>
