5bb7e9d7e7386c8e6da7bec8c30e57b329d3b3a4
kelkel17 <saturre.mic2@gmail.com>
2018-02-20 03:51:04 +0800

    updated updateadmin.php allow no logo if there's already a logo

--- Controller/RestaurantsController/updateadmin.php
+++ Controller/RestaurantsController/updateadmin.php
@@ -5,7 +5,7 @@
     	$id = $_POST['id'];
     	$user = $_POST['username'];
     	$pass = $_POST['password'];
-    	$pass2 = $_POST['password2'];
+    	//$pass2 = $_POST['password2'];
     	$name = $_POST['rname'];
     	$addr = $_POST['addr'];
     	$contact = $_POST['contact'];
@@ -19,10 +19,15 @@
 		$checkbox2 = "";
 		$directory = "../../Image/";
 		$path = time().$image;
-		  	if(move_uploaded_file($_FILES['image']['tmp_name'], $directory.$path))
-		  	{
-		  		
-		  			$data = array($user,$pass,$name,$addr,$contact,$otime,$ctime,$path,$desc,$max,$email,$id);
+		if(!empty($image))
+            $path = time().$image;
+		else  $path = '';
+		if(empty($path))
+			$data = array($user,$pass,$name,$addr,$contact,$otime,$ctime,$desc,$max,$email,$id);
+		  	else{
+				  if(move_uploaded_file($_FILES['image']['tmp_name'], $directory.$path))
+				  	{
+						$data = array($user,$pass,$name,$addr,$contact,$otime,$ctime,$path,$desc,$max,$email,$id);
 		  				foreach ($promo as $check) {
 			  				$sql = "SELECT promo_id, restaurant_id FROM promo_restaurant WHERE promo_id = :promo AND restaurant_id = :id";
 				            $con = con();
@@ -32,15 +37,15 @@
 				            $sthandler->execute();
 
 				            if($sthandler->rowCount() > 0)
-				            	  echo '<script>alert("You already availed this promo!"); window.location="../../View/updaterestaurant.php?id='.$id.'";</script>';
-				             else
-		  							$data2 = array($check,$id);
-		  				 }
-						  updateOwner($data);
-					 	  addPromo($data2);
-					  echo '<script> alert("Succesfully Updated your restaurant information"); window.location="../../View/updaterestaurant.php?id='.$id.'";</script>';
-		  	
-		  	}
-		
+				            	 echo '<script>alert("You already availed this promo!"); window.location="../../View/updaterestaurant.php?id='.$id.'";</script>';
+							 else{
+								 addPromo($data2); 
+								 $data2 = array($check,$id);
+							}	 
+						 }
+					}		   
+			  }	 
+			  updateOwner($data,$path);
+			 echo '<script> alert("Succesfully Updated your restaurant information"); window.location="../../View/updaterestaurant.php?id='.$id.'";</script>';
 	}	
 ?>
