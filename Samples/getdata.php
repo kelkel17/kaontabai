aa746707fb2bfa3f9a28cceba0f318956908c6ec
kelkel17 <saturre.mic2@gmail.com>
2018-02-20 02:56:50 +0800

    update getdata.php

--- Samples/getdata.php
+++ Samples/getdata.php
@@ -1,7 +1,7 @@
 <?php
     include('../Controller/dbconn.php');
     $con = con();
-    $sql = "SELECT count(*) as count, reservation_date as dat, reservation_time as tim FROM reservations WHERE res_status = 'Pending' AND restaurant_id = ?";
+    $sql = "SELECT count(*) as count, reservation_date as dat, reservation_time as tim FROM reservations WHERE res_status = 'Pending' AND restaurant_id = 1";
     $stmt = $con->prepare($sql);
     $stmt->execute();
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
