7e58b888f87817f6677c92aa5d24c94defbdbdce
kelkel17 <saturre.mic2@gmail.com>
2018-02-19 13:55:14 +0800

    updated getdate.php

--- Model/Customer/getdate.php
+++ Model/Customer/getdate.php
@@ -1,7 +1,7 @@
 <?php
     include('../../Controller/dbconn.php');
     $con = con();
-    $id = 1;
+    $id = $_GET['cid'];
     $sql = "SELECT sched_sdate as dat FROM schedules WHERE status = 1 AND restaurant_id = 1";
     $stmt = $con->prepare($sql);
     $stmt->execute();
