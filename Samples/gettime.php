141db1cd64de6480563d62110fd56c86f870f5a5
kelkel17 <saturre.mic2@gmail.com>
2018-02-20 13:59:57 +0800

    added new sample

---
+++ Samples/gettime.php
@@ -0,0 +1,18 @@
+<?php
+include '../Controller/dbconn.php';
+header('Content-Type: application/json');
+
+	$con = con();
+    $id = $_GET['cid'];
+    // $id = 1;
+	//$sql = sprintf("SELECT * FROM subscriptions");
+	$query = "SELECT hour_open as open, hour_close as close from restaurants where restaurant_id = '$id'";
+	$data = $con->prepare($query);
+	$data->execute(); 
+	$info = $data->fetch(PDO::FETCH_ASSOC);
+	$data = array();
+		$data[] = $info;
+
+	print json_encode($data);
+?>
+
