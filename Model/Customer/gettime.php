be3e4605e9daadcb1102f525067a5d9fdfd6b47e
kelkel17 <saturre.mic2@gmail.com>
2018-02-20 13:58:41 +0800

    updated gettime.php

--- Model/Customer/gettime.php
+++ Model/Customer/gettime.php
@@ -2,16 +2,17 @@
 include '../Controller/dbconn.php';
 header('Content-Type: application/json');
 
-	$con = con();
-	//$sql = sprintf("SELECT * FROM subscriptions");
-	$query = "SELECT hour_open as open, hour_close as open from restaurants where restaurant_id = ?";
-	$data = $con->prepare($query);
-	$data->execute(); 
-	$info = $data->fetch(PDO::FETCH_ASSOC);
-	$data = array();
-	foreach ($info as $ro) {
-		$data[] = $ro;
-	}
-	print json_encode($data);
+$con = con();
+$id = $_GET['cid'];
+// $id = 1;
+//$sql = sprintf("SELECT * FROM subscriptions");
+$query = "SELECT hour_open as open, hour_close as close from restaurants where restaurant_id = '$id'";
+$data = $con->prepare($query);
+$data->execute(); 
+$info = $data->fetch(PDO::FETCH_ASSOC);
+$data = array();
+	$data[] = $info;
+
+print json_encode($data);
 ?>
 
