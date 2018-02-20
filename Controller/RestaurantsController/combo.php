f7c261f1724eb585b249790d06081ee12590df3f
kelkel17 <saturre.mic2@gmail.com>
2018-02-21 06:30:07 +0800

    updated combo meal add

--- Controller/RestaurantsController/combo.php
+++ Controller/RestaurantsController/combo.php
@@ -9,33 +9,48 @@
 		$desc = $_POST['desc'];
 		$image = $_FILES['image']['name'];
 		$directory = "../../Image/";
+		if(!(empty($image)))
 		$path = time().$image;
+		else
+		$path = '';
 		$imageType = strtolower(pathinfo($image,PATHINFO_EXTENSION));
-            if($imageType != "jpg" && $imageType != "png" && $imageType != "jpeg"){
-                echo '<script> alert("Image must be a JPG/JPEG/PNG"); window.location="../../Model/Restaurant/combo.php?e=Invalid extension&style=danger&head=Oh snap!"; </script>';
-            }else{		
-			  		if(move_uploaded_file($_FILES['image']['tmp_name'], $directory.$path))
-			  		{
-			  					$sql= "SELECT cm_name FROM combo_meals WHERE cm_name = :name";
-			  					$con = con();
-			  					$sthandler = $con->prepare($sql);
-			  					$sthandler->bindParam(':name', $name);
-			  					$sthandler->execute();
-			  					if($sthandler->rowCount() > 0){
-			  							 echo '<script>alert("You already availed this Combo meal!"); window.location="../../Model/Restaurant/promo.php?id='.$id.'";</script>';
-			  					}else{
-			  						foreach ($menu as $men) {
-										$data = array($id,$name,$desc,$price,$path,$number,$men);
-										addCombo($data);
-									} 	
-									echo '<script> alert("Successfully Added a Combo meal"); window.location="../../Model/Restaurant/promo.php" </script>';		
+            if($imageType != "jpg" && $imageType != "png" && $imageType != "jpeg" && !(empty($path)))
+                echo '<script> alert("Image must be a JPG/JPEG/PNG"); window.location="../../Model/Restaurant/promo.php?e=Invalid extension&style=danger&head=Oh snap!"; </script>';
+            else{		
+				
+				
+							if(move_uploaded_file($_FILES['image']['tmp_name'], $directory.$path))
+							{
+										$sql= "SELECT cm_name FROM combo_meals WHERE cm_name = :name";
+										$con = con();
+										$sthandler = $con->prepare($sql);
+										$sthandler->bindParam(':name', $name);
+										$sthandler->execute();
+										if($sthandler->rowCount() > 0)
+												echo '<script>alert("You already availed this Combo meal!"); window.location="../../Model/Restaurant/promo.php?id='.$id.'";</script>';
+										else{
+											$men = implode(",",$menu);
+											// echo $men;
+											// foreach ($menu as $men) {
+												if(empty($path))
+													$data = array($id,$name,$desc,$price,$number,$men);
+												else
+													$data = array($id,$name,$desc,$price,$path,$number,$men);
+												
+												addCombo($data,$path);
+												echo '<script> alert("Successfully Added a Combo meal"); window.location="../../Model/Restaurant/promo.php" </script>';	
+											// } 	
+												
+										}
+										
+										
 							}
+							else
+								echo '<script> alert("Error in adding a Combo meal"); window.location="../../Model/Restaurant/combo.php" </script>';
 								
-								
-					}else{
-						echo '<script> alert("Error in adding a Combo meal"); window.location="../../Model/Restaurant/combo.php" </script>';
-					}
 			}
+				
+				
 	}
 
 	if(isset($_POST['update'])){
@@ -48,39 +63,40 @@
 		$desc = $_POST['desc'];
 		$image = $_FILES['image']['name'];
 		$directory = "../../Image/";
+	
+		if(!(empty($image)))
 		$path = time().$image;
+		else
+		$path = '';
 		$imageType = strtolower(pathinfo($image,PATHINFO_EXTENSION));
-              if($imageType != "jpg" && $imageType != "png" && $imageType != "jpeg"){
-                echo '<script> alert("Image must be a JPG/JPEG/PNG"); window.location="../../Model/Restaurant/combo.php?e=Invalid extension&style=danger&head=Oh snap!"; </script>';
-            }else{		
-			  		if(move_uploaded_file($_FILES['image']['tmp_name'], $directory.$path))
-			  		{
-			  					$sql= "SELECT * FROM combo_meals WHERE cm_id = :cid AND restaurant_id = :id AND cm_number = :cmid";
-			  					$con = con();
-			  					$sthandler = $con->prepare($sql);
-			  					$sthandler->bindParam(':cid', $cim);
-			  					$sthandler->bindParam(':id', $id);
-			  					$sthandler->bindParam(':cmid', $cm);
-			  					$sthandler->execute();
-			  					if($sthandler->rowCount() > 0){
+            if($imageType != "jpg" && $imageType != "png" && $imageType != "jpeg" && !(empty($path)))
+			echo '<script> alert("Image must be a JPG/JPEG/PNG"); window.location="../../Model/Restaurant/promo.php?e=Invalid extension&style=danger&head=Oh snap!"; </script>';
+            else{		
+					
+				
+							if(move_uploaded_file($_FILES['image']['tmp_name'], $directory.$path))
+							{
+								
+									
+										
+											$men = implode(",",$menu);
+											echo $men;
+												if(empty($path)){
+													$data = array($id,$name,$desc,$price,$men,$cim);
+													updateCombo($data,$path);
+												}
+												else{
+													$data = array($id,$name,$desc,$price,$path,$men,$cim);
+													updateCombo($data,$path);
+												}
+													
+												// echo '<script> alert("Successfully updated a Combo meal"); window.location="../../Model/Restaurant/promo.php" </script>';	
 
-			  						foreach ($menu as $men) {
-			  							$data = array($id,$name,$desc,$price,$path,$cm,$men);
-										addCombo($data);	
-									}
-									//echo '<script> alert("Successfully updated a Combo meal"); window.location="../../Model/Restaurant/promo.php" </script>';	
-			  					}else{
-			  						foreach ($menu as $men) {
-			  							$data = array($id,$name,$desc,$price,$path,$cm);
-			  							updateCombo($data);
-									} 	
-									//echo '<script> alert("Successfully updated a Combo meal"); window.location="../../Model/Restaurant/promo.php" </script>';		
+										
 							}
+							// else
+							// 	echo '<script> alert("Error in updating a Combo meal"); window.location="../../Model/Restaurant/promo.php" </script>';
 								
-								
-					}else{
-						//echo '<script> alert("Error in upadting a Combo meal"); window.location="../../Model/Restaurant/combo.php" </script>';
-					}
 			}
 	}
 ?>
