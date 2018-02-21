9594758599235bb5bc8f4d075aacaed3f409aaeb
kelkel17 <saturre.mic2@gmail.com>
2018-02-22 05:49:03 +0800

    updated story

--- Model/Restaurant/story.php
+++ Model/Restaurant/story.php
@@ -140,6 +140,17 @@
     } );
 } );
  </script>
-			
+	<script>
+function printDiv(divName) {
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+}
+</script>		
 </body>
 </html>
