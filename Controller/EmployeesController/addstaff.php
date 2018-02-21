8a176064a47bb8fc805ac9f146e8d5a2019731e0
kelkel17 <saturre.mic2@gmail.com>
2018-02-21 18:18:02 +0800

    added sweet alert

--- Controller/EmployeesController/addstaff.php
+++ Controller/EmployeesController/addstaff.php
@@ -1,3 +1,73 @@
+<script src="../../something/js/jquery.min.js"></script>
+<script src="../../something/js/sweetalert.min.js"></script>
+<script>
+	function sweetMimitch(){
+	$(function(){
+		swal({
+				title:"Successfully",
+				text:"Added a staff",
+				icon: "success"
+		}).then(function(){
+				window.location = "../../Model/Employee/staff.php";
+			});
+		});
+	}
+	function updateAlert(){
+		$(function(){
+			swal({
+				title:"Successfully",
+				text:"Updated a staff",
+				icon: "success"
+			}).then(function(){
+					window.location = "../../Model/Employee/staff.php";
+			});
+		});
+	}
+	function errorUpdateAlert(){
+		$(function(){
+			swal({
+				title:"Error",
+				text:"Error in Updating a staff",
+				icon: "error"
+			}).then(function(){
+					window.location = "../../Model/Employee/staff.php";
+			});
+		});
+	}
+	function warningAlert(){
+		$(function(){
+			swal({
+				title:"Image type error",
+				text:"Image type must be PNG/JPEG/JPG only",
+				icon: "Warning"
+			}).then(function(){
+					window.location = "../../Model/Employee/staff.php";
+			});
+		});
+	}
+	function warningAlert2(){
+		$(function(){
+			swal({
+				title:"Error in adding a staff",
+				text:"staff already exist",
+				icon: "Error"
+			}).then(function(){
+					window.location = "../../Model/Employee/staff.php";
+			});
+		});
+	}
+	function errorAlert(){
+		$(function(){
+			swal({
+				title:"Error in adding a staff",
+				text:"staff already exist",
+				icon: "Error"
+			}).then(function(){
+					window.location = "../../Model/Employee/staff.php";
+			});
+		});
+	}
+</script>
 <?php
 
 include '../dbconn.php';
@@ -23,13 +93,29 @@ include '../dbconn.php';
 			$sthandler->bindParam(':username', $username);
 			$sthandler->execute();
 
-			if(count($sthandler) > 1){
-			    echo '<script>alert("Staff Already Exist!"); window.location="../../Model/Employee/staff.php" </script>';
+			if($sthandler->rowCount() > 0){
+			    echo'<script> warningAlert2(); </script>';
 			} else{
 				$data = array($id,$number,$fname,$mname,$lname,$addr,$phone,$gender,$ssn,$username,$password);
 				//echo $id,$fname,$mname,$lname,$addr,$phone,$gender,$ssn,$username,$password;
 				addStaff($data);
+				echo '<script>sweetMimitch();</script>';
 			}
 	}
-			$con = null;
+	if(isset($_POST['updateStaff'])){
+		$id = trim($_POST['id']);
+		$fname = trim($_POST['fname']);	
+		$mname = trim($_POST['mname']);			
+		$lname = trim($_POST['lname']);
+		$addr = trim($_POST['address']);
+		$phone = trim($_POST['phone']);
+		$gender = trim($_POST['gender']);
+		$ssn = trim($_POST['ssn']);
+				
+					$data = array($fname,$mname,$lname,$addr,$phone,$gender,$ssn,$id);
+					updateStaff($data);
+					echo '<script>updateAlert();</script>';
+			
+	
+  	}
 ?>
