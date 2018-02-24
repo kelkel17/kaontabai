56a6cdc019339168f368318d1994018a3d33fc9b
kelkel17 <saturre.mic2@gmail.com>
2018-02-25 02:00:31 +0800

    deactivate status not done yet

--- Controller/SchedulesController/deactivateschedule.php
+++ Controller/SchedulesController/deactivateschedule.php
@@ -2,29 +2,20 @@
 	include '../dbconn.php';
 
 	if(isset($_POST['off'])){
-		$id = $_POST['id'];
+		$id = $_POST['off'];
 		$stat = 0;
 		$data = array($stat,$id);
 		deactivateSchedule($data);
-		header("Location: ../../Model/Schedule/schedules.php");
+		//header("Location: ../../Model/Schedule/schedules.php");
 	}
 
 	if(isset($_POST['peak'])){
-		$id = $_POST['id'];
+		$id = $_POST['peak'];
 		$stat = 1;
 		$data = array($stat,$id);
 		deactivateSchedule($data);
-		header("Location: ../../Model/Schedule/schedules.php");
+		//header("Location: ../../Model/Schedule/schedules.php");
 	}
 
-	if(isset($_POST['update'])){
-		$id = $_POST['id'];
-		$rid = $_SESSION["id"];
-		$sdate = $_POST['sdate'];
-		$stime = $_POST['stime'];
-		$edate = $_POST['edate'];
-		$etime = $_POST['etime'];
-		$data = array($rid,$sdate,$stime,$edate,$etime,$id);
-		updateSchedule($data);
-	}
+
 ?>
