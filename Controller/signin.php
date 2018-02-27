8ae9e021dd5b59fb6f0441686f331f344a470502
kelkel17 <saturre.mic2@gmail.com>
2018-02-27 13:48:30 +0800

    added password verify for cstomer

--- Controller/signin.php
+++ Controller/signin.php
@@ -1,26 +1,26 @@
 <?php
 include 'dbconn.php';
-	//session_start();
+//	session_start();
 
-	//$email = $password = "";
+	//$userlog = $userpass = "";
 	if(isset($_POST['login']))
 	{
 		$email = $_POST['email'];
 		$password = $_POST['password'];
-	
-		$sql = "SELECT * FROM customers WHERE customer_email = ? AND customer_password = ?";
+
+		$sql = "SELECT * FROM customers WHERE customer_email = ?";
 		$con = con();
 		$stmt = $con->prepare($sql);
-		$stmt->execute(array($email,$password));
+		$stmt->execute(array($email));
 		$row = $stmt->fetch(PDO::FETCH_ASSOC);
-		if($stmt->rowCount() > 0){
-			$_SESSION['id'] = $row['customer_id'];
-			echo '<script>window.location="../Model/Customer/loading.php?id='.$row['customer_id'].'" </script>';
-		
+		if($stmt->rowCount() > 0 && password_verify($password, $row['customer_password'])){
+				$_SESSION['id'] = $row['customer_id'];
+				echo '<script>window.location="../Model/Customer/loading.php?id='.$row['customer_id'].'" </script>';
 		}
 		else{
 			echo '<script> alert("Invalid Username or Password"); window.location="../index.php?mess=Your username or password is incorrect!" </script>';
 		}
-		
-}		
+
+	}
+		//$con=null;
 ?>
