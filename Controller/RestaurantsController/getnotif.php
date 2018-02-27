f29c423521d7b513fa9bd6d7269a33959df86805
kelkel17 <saturre.mic2@gmail.com>
2018-02-27 13:47:52 +0800

    re query notifcation

--- Controller/RestaurantsController/getnotif.php
+++ Controller/RestaurantsController/getnotif.php
@@ -1,9 +1,9 @@
 <?php
     include('../dbconn.php');
     $con = con();
+    $id = 1;
     // $id = $_SESSION['id'];
-    $id = $_SESSION['id'];
-    $sql = "SELECT count(*) as count, DATE_FORMAT(time_receive, '%M %d, %Y') as dat FROM notifications WHERE status = 0 AND restaurant_id = '$id' GROUP BY restaurant_id";
+    $sql = "SELECT count(*) as count, time_receive as dat FROM notifications WHERE status = 0 AND restaurant_id = '$id' GROUP BY restaurant_id";
     $stmt = $con->prepare($sql);
     $stmt->execute();
     $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
