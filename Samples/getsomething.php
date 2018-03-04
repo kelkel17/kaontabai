f4a35a898b2e0ba645fcb08d157496333979325d
kelkel17 <saturre.mic2@gmail.com>
2018-03-05 03:09:21 +0800

    update

--- Samples/getsomething.php
+++ Samples/getsomething.php
@@ -2,20 +2,36 @@
 include '../Controller/dbconn.php';
 header('Content-Type: application/json');
 
-// $id = $_SESSION['id'];
-$id = 1;
+// if(isset($_POST['sales'])){
 	$con = con();
-	//$sql = sprintf("SELECT * FROM subscriptions");
-	$query = "SELECT SUM(o.total_price) as total, m.m_name as name, o.order_time as tim FROM menus as m, order_details as od, orders as o WHERE m.menu_id = od.menu_id AND od.order_id = o.order_id AND o.restaurant_id = '$id' GROUP BY date_format(o.order_time, '%d') ORDER BY od.menu_id LIMIT 2";
+	$menu = 1;
+	$sdate = date('2018-03-03');
+	$edate = date('2018-03-11');
+	$newsdate = date('F j, Y',strtotime($sdate));
+	$newedate = date('F j, Y',strtotime($edate));
+	echo $newsdate.'<br>';
+	echo $newedate.'<br>';
+	// $sdate = date('F j, Y',strtotime($_POST['sdate']));
+	// $edate = date('F j, Y',strtotime($_POST['edate']));
+	$query = "SELECT (od.order_qty * m.m_price) as total, o.order_time as daytime FROM menus m, orders o, order_details od WHERE m.menu_id = od.menu_id AND od.menu_id = '$menu' AND od.order_id = o.order_id";
 	$data = $con->prepare($query);
 	$data->execute(); 
 	$info = $data->fetchAll(PDO::FETCH_ASSOC);
 	$data = array();
 	foreach ($info as $ro) {
-		$data[] = $ro;
+		$newdate = date('F j, Y', strtotime($ro['daytime']));
+		// $data[] = $ro;
+		if($newsdate >= $newdate && $newedate <= $newdate){
+			$data[] = $ro;
+			// echo $data[];
+		}
+		else{
+			echo 'wala';
+		}	
 	}
 	print json_encode($data);
+	
 // $sql = "SELECT  SUM(p.purchase_total) as amount FROM tbl_purchase_details as pd, tbl_purchase as p, tbl_products as pr  WHERE pd.purchase_id = p.purchase_id AND pd.product_id = pr.product_id";
-   
+// }
 ?>
 
